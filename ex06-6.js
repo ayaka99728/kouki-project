d5921182b3d2e57a5f16678101c7def7edcbcf78
ayaka99728 <zm18500@josai.ac.jp>
2021-01-26 03:18:28 +0000

    01.26

--- ex06-6.js
+++ ex06-6.js
@@ -1,1 +1,3 @@
 function imgchange1() {document.getElementById('logo').src= './image/gurando_Image.jpg';}
+
+function ChangeImageAndBack() {document.getElementById('img13').src= './image/kansya.jpeg';}
